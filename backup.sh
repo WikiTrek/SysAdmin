be86c0af49d875178867605880a7da22d8dec3e2
Luca Mauri <lucamauri@users.noreply.github.com>
2020-11-02 23:38:21 +0100

    Update backup.sh
    New version in operation since move to Hetzner

--- backup.sh
+++ backup.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
-# Backup database and folder for WikiTrek
+# Summary: Backup database and folder for WikiTrek
+# Usage: sh backup RootDBPassword
+# Author: Luca Mauri
+# -------------------------------------------------
 
-WeekNR = $((($(date +%-d)-1)/7+1))
+WeekNR=$((($(date +%-d)-1)/7+1))
+BackupDir="/var/backups/trek/"
 
-mysqldump --user=wtstaff -ppswd wikitrek > /var/backups/$WeekNRwikitrekorg.sql
-mysqldump --user=wtstaff -ppswd wikitrek --xml > /var/backups/$WeekNRwikitrekorg.xml
+for DBName in wikitrek datatrek blogtrek
+do
+    echo "SQL Backup: $DBName"
+    sudo mysqldump -u root -p"$1" "$DBName" > "$BackupDir$WeekNR$DBName".sql
+    echo "XML Backup: $DBName"
+    sudo mysqldump -u root -p"$1" "$DBName" --xml > "$BackupDir$WeekNR$DBName".xml	
+done
 
-tar -cvf /var/backups/$WeekNRwikitrekorg.tar /var/www/wikitrek
+echo
+echo "TAR misc"
+tar -cf "$BackupDir$WeekNR"apache.tar /etc/apache2/sites-available/
+tar -cf "$BackupDir$WeekNR"encrypt.tar /etc/letsencrypt/live/
+tar -cf "$BackupDir$WeekNR"documents.tar /home/lmauri/documents/
+echo
+echo "TAR WWW"
+tar -cf "$BackupDir$WeekNR"www.tar /var/www/
 
-gzip /var/backups/$WeekNRwiki*
+echo
+echo "GZIPping"
+gzip "$BackupDir"*
